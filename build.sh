d3fc6450ce667762652f4832d68e95998865ff4e
code-with-ojas <abhinavanand9996@gmail.com>
2026-09-09 01:15:20 +0530

    Optimize build.sh for Render with CPU-only PyTorch

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install --upgrade pip
+pip install --no-cache-dir torch --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
 python -m spacy download en_core_web_md || python -m spacy download en_core_web_sm
 python -c "from sentence_transformers import SentenceTransformer; SentenceTransformer('all-MiniLM-L6-v2')"
